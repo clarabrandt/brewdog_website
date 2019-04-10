ab55e9111ed490ab94a2edf2a41edaeb37856476
clarabrandt <mariaclarabrandt@gmail.com>
2019-04-10 19:29:12 +0200

    Including menu in app as a component

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <nav class="navbar is-transparent">
-        <div class="navbar-brand">
-          <a class="navbar-item" href="https://bulma.io"></a>
-          <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        </div>
-        <div id="navbarExampleTransparentExample" class="navbar-menu">
-          <div class="navbar-start">
-            <div class="dropdown is-active">
-              <div class="dropdown-trigger">
-                <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
-                  <span>Beers</span>
-                  <span class="icon is-small">
-                    <i class="fas fa-angle-down" aria-hidden="true"></i>
-                  </span>
-                </button>
-              </div>
-              <div class="dropdown-menu" id="dropdown-menu" role="menu">
-                <div class="dropdown-content">
-                  <div class="dropdown-item">Punk IPA</div>
-                  <div class="dropdown-item">Dead Pony Club</div>
-                  <div class="dropdown-item is-active">Indie</div>
-                  <div class="dropdown-item">Lost Lager</div>
-                  <div class="dropdown-item">Clockwork Tangerine</div>
-                  <div class="dropdown-item">Jet Black Heart</div>
-                  <div class="dropdown-item">Quench Quake</div>
-                </div>
-              </div>
-            </div>
-            <span class="navbar-item">
-              <router-link to="/home">Home</router-link>
-            </span>
-            <span class="navbar-item">
-              <router-link to="/beers">Beers</router-link>
-            </span>
-          </div>
-
-          <div class="navbar-end">
-            <div class="navbar-item">oi</div>
-          </div>
-        </div>
+      <Menu />
       </nav>
     </div>
 
@@ -65,6 +22,20 @@
     </footer>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import Menu from '@/components/Menu.vue' // @ is an alias to /src
+
+
+@Component({
+  components: {
+    Menu
+  }
+})
+export default class App extends Vue {}
+</script>
+
 
 <style>
 #app {
