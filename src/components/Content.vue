7034e9af177b71a1634dbee7f03c0b01a176c2be
clarabrandt <mariaclarabrandt@gmail.com>
2019-04-10 19:24:31 +0200

    creating a content component

---
+++ src/components/Content.vue
@@ -0,0 +1,15 @@
+<template class="content">
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Content extends Vue {
+  @Prop() private msg!: string
+}
+</script>
+
