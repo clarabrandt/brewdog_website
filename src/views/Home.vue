a711bcab28cab8c8c8d91d46080c55aaae14a271
clarabrandt <mariaclarabrandt@gmail.com>
2019-04-10 19:22:08 +0200

    First commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <!-- <img alt="Brewdog logo" src="../assets/brewdog-logo.png"> -->
+    <Banner msg="The Brewdog Beer World"/>
+    <div class="more-content">
+      <Content msg="The best beers in the world"/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Component, Vue } from 'vue-property-decorator'
+import Banner from '@/components/Banner.vue' // @ is an alias to /src
+import Content from '@/components/Content.vue' // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld
+    Banner,
+    Content
   }
 })
 export default class Home extends Vue {}
 </script>
+
+<style>
+.home {
+  height: 100%;
+}
+</style>
