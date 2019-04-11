155f499095f071142c835092e2b217950261e939
clarabrandt <mariaclarabrandt@gmail.com>
2019-04-11 16:03:19 +0200

    styling footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="footer">
     <div class="content has-text-centered">
-      <p>
+      <h4>
         <strong>Bulma</strong> by
         <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
         <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
@@ -9,7 +9,7 @@
         <a
           href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
         >CC BY NC SA 4.0</a>.
-      </p>
+      </h4>
     </div>
   </footer>
 </template>
