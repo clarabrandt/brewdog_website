c354352ffd3ef5696e37b619f45f0a28991b2470
clarabrandt <mariaclarabrandt@gmail.com>
2019-04-11 17:21:13 +0200

    estructuring description of beer

---
+++ src/components/IPA.vue
@@ -0,0 +1,33 @@
+<template class="IPA">
+  <div class='ipa section'>
+    <div class='ipa-type columns'>
+      <div class='ipa-type-content column'>
+        <img></img>
+      </div>
+      <div class='ipa-type-content column'>
+        <div>NAME</div>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+      </div>
+      <div class='ipa-type-content column'>
+        <div>Tagline</div>
+        <div>ABV</div>
+        <div>IBU</div>
+        <div>Malts</div>
+        <div>Hops</div>
+        <div>Food paring</div>
+      </div>
+     </div> 
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class IPA extends Vue {
+  @Prop() private msg!: string
+}
+</script>
