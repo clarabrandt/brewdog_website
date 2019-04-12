992a5e6f3fb328ea34c0a744e6a0ac887a9d9263
clarabrandt <mariaclarabrandt@gmail.com>
2019-04-12 13:10:16 +0200

    new component BeerList

---
+++ src/components/BeerList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class BeerList extends Vue {
+  @Prop() private msg!: string
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
