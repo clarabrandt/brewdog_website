dfaa3f1ca53e1479293ef86f65d7706ca89a5504
clarabrandt <mariaclarabrandt@gmail.com>
2019-04-18 09:06:58 +0200

    new design and pics

---
+++ src/components/BeerDetails.vue
@@ -0,0 +1,93 @@
+<template class="IPA">
+  <!-- <div v-for="beer in oneBeer" :key="beer.id" class="eachBeer">{{ beer.name }}</div> -->
+
+  <div class="ipa section">
+    <div class="ipa-type columns">
+      <!-- <div class="ipa-type-content column">
+        <img src="../assets/storm_ipa.jpg" alt="Brewdog logo" width="150" height="250">
+      </div>-->
+      <div class="ipa-type-content column">
+        <div
+          v-for="beer in oneBeer"
+          :key="beer.id"
+          class="ipa-name has-text-weight-bold"
+        >{{ beer.name }}</div>
+        <div class="ipa-tagline">Islay Whisky Aged IPA</div>
+        <div class="ipa-description has-text-justified">
+          Dark and powerful Islay magic infuses this tropical sensation of an IPA. Using the original Punk IPA as a base,
+          we boosted the ABV to 8% giving it some extra backbone to stand up to the peated smoke imported directly from Islay.
+        </div>
+      </div>
+      <div class="ipa-type-content characteristics column has-text-left">
+        <div
+          v-for="beer in oneBeer"
+          :key="beer.id"
+          class="ipa-characteristic top"
+        >abv: {{ beer.abv }}</div>
+        <div v-for="beer in oneBeer" :key="beer.id" class="ipa-characteristic">id: {{ beer.id }}</div>
+        <div v-for="beer in oneBeer" :key="beer.id" class="ipa-characteristic">ibu: {{ beer.ibu }}</div>
+        <div class="ipa-characteristic">Malts</div>
+        <div class="ipa-characteristic">Hops</div>
+        <div class="ipa-characteristic">Food paring: oysters, hickory smoked ham, rocky road</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import { mapState, mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'BeerDetails',
+
+  methods: {
+    ...mapActions(['fetchRandom'])
+  },
+
+  computed: {
+    ...mapState(['randomBeer']),
+    ...mapGetters(['oneBeer'])
+  },
+  created() {
+    this.fetchRandom()
+  }
+}
+</script>
+<style>
+.ipa {
+  background-color: #b26624;
+}
+.ipa-type {
+  background-color: #454140;
+  /* margin: 0 10px */
+}
+.ipa-type-content {
+  height: 100%;
+}
+.ipa-name {
+  margin-top: 50px;
+  font-size: 30px;
+  color: #b26624;
+}
+.ipa-tagline {
+  color: #b26624;
+  font-size: 20px;
+}
+.ipa-description {
+  color: black;
+  margin: 40px;
+  font-size: 20px;
+}
+.ipa-characteristic {
+  color: #b26624;
+  margin: 10px;
+  font-size: 20px;
+}
+.top {
+  margin-top: 130px;
+}
+.characteristics {
+  padding: 10px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="home">
     <!-- <img alt="Brewdog logo" src="../assets/brewdog-logo.png"> -->
-    <Banner msg="The Brewdog Beer World"/>
+    <Banner msg="The Brewdog Beer World" class="cd-fixed-bg cd-fixed-bg--1"/>
     <div class="more-content">
-      <div>What do you need to make the perfect beer?</div>
-      <div class="columns is-2-desktop">
-        <div class="column has-background-link">Water</div>
-        <div class="column has-background-success">Hops</div>
-        <div class="column has-background-dark">Malt</div>
-        <div class="column has-background-warining">Yeast</div>
-        <div class="column">The Perfect Brewdog Recipe!</div>
+      <div class="cd-scrolling-bg cd-scrolling-bg--color-0">
+        <div class="ingredient cd-scrolling-bg__content">
+          <p>What do you need to make the perfect beer?</p>
+        </div>
+      </div>
+      <div class="cd-fixed-bg cd-fixed-bg--2">
+        <h1>WATER</h1>
+      </div>
+      <div class="cd-fixed-bg cd-fixed-bg--3 column">
+        <h1>HOPS</h1>
+      </div>
+      <div class="cd-fixed-bg cd-fixed-bg--4 column">
+        <h1>HOPS</h1>
+      </div>
+      <div class="cd-fixed-bg cd-fixed-bg--5 column">
+        <h1>HOPS</h1>
+      </div>
+      <div class="cd-fixed-bg cd-fixed-bg--6 column">
+        <h1>HOPS</h1>
       </div>
     </div>
   </div>
@@ -37,5 +49,34 @@ export default class Home extends Vue {}
 .columns {
   height: 300px;
   /* width: 400px; */
+}
+.cd-fixed-bg {
+  min-height: 100vh;
+  background-size: cover;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-position: center center;
+}
+
+.cd-fixed-bg.cd-fixed-bg--1 {
+  background-image: url('../assets/brewdog.jpg');
+}
+
+.cd-fixed-bg.cd-fixed-bg--2 {
+  background-image: url('../assets/water2.jpg');
+  background-size: 50%;
+  background-position-x: left;
+  /* max-width: 50%; */
+  height: 200px;
+}
+.cd-fixed-bg.cd-fixed-bg--3 {
+  background-image: url('../assets/hops2.jpg');
+  background-size: 50%;
+  max-width: 100%;
+  background-position-x: right;
+}
+
+.cd-scrolling-bg {
+  min-height: 100%;
 }
 </style>
