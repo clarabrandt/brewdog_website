9995cbb7de8b160f5e7ca755d12ee07255e9142e
clarabrandt <mariaclarabrandt@gmail.com>
2019-04-23 12:30:32 +0200

    including items on dropdown menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,15 +1,4 @@
 <template class="menu">
-  <!-- <div class="menu-items">
-    <div class="menu-item">Home</div>
-    <div class="menu-item menu-beers">
-      <a href="#" v-on:click.prevent="showDropDown=!showDropDown">
-      <div>Beers</div>
-      <i :class="{ 'fa-caret-up':showDropDown, 'fa-caret-down': !showDropDown }" class="fa" aria-hidden="true"></i>
-      </a>
-    </div>
-    <div class="menu-item">Random</div>
-  </div>-->
-
   <div class="navbar">
     <div class="menu-items">
       <div class="menu-items--left">
@@ -23,20 +12,61 @@
             <div>Home</div>
           </router-link>
         </div>
-        <div class="menu-item dropdown">
-          <router-link to="/beers">
-            <div @mouseover="toggleDropdown" @mouseout="toggleDropdown">Beers</div>
-          </router-link>
+        <div class="menu-item">
+          <div>
+            <router-link to="/beers">
+              <div @mouseover="toggleDropdown" @mouseout="toggleDropdown">Beers</div>
+            </router-link>
+            <div class="arrow"></div>
+          </div>
           <div
             class="menu-item-dropdown"
             v-show="active"
             @mouseover="toggleDropdown"
             @mouseout="toggleDropdown"
           >
-            <div>
-              <router-link to="/beers/abv/0/3">item 1</router-link>
+            <div class="abv">
+              <div class="menu-item-title">ABV</div>
+              <router-link to="/beers/abv/0/3">
+                <a class="navbar-item has-text-weight-bold is-size-6" value="0-3">0 - 3%</a>
+              </router-link>
+              <router-link to="/beers/abv/3/6">
+                <a class="navbar-item has-text-weight-bold is-size-6" value="3-6">3 - 6%</a>
+              </router-link>
+              <router-link to="/beers/abv/6/9">
+                <a class="navbar-item has-text-weight-bold is-size-6" value="6-10">6 - 9%</a>
+              </router-link>
+              <router-link to="/beers/abv/9/15">
+                <a class="navbar-item has-text-weight-bold is-size-6" value="9-15">9 - 15%</a>
+              </router-link>
+              <router-link to="/beers/abv/15/35">
+                <a class="navbar-item has-text-weight-bold is-size-6" value="15-25">15 - 35%</a>
+              </router-link>
+              <router-link to="/beers/abv/35/60">
+                <a class="navbar-item has-text-weight-bold is-size-6" value="16">> 35%</a>
+              </router-link>
             </div>
-            <div>item 2</div>
+            <div class="ibu">
+              <div class="menu-item-title">IBU</div>
+              <router-link to="/beers/ibu/0/20">
+                <a class="navbar-item has-text-weight-bold is-size-6" value="0-20">0 - 20</a>
+              </router-link>
+              <router-link to="/beers/ibu/20/40">
+                <a class="navbar-item has-text-weight-bold is-size-6" value="20">20 - 40</a>
+              </router-link>
+              <router-link to="/beers/ibu/40/60">
+                <a class="navbar-item has-text-weight-bold is-size-6" value="40">40 - 60</a>
+              </router-link>
+              <router-link to="/beers/ibu/60/80">
+                <a class="navbar-item has-text-weight-bold is-size-6" value="60">60 - 80</a>
+              </router-link>
+              <router-link to="/beers/ibu/80/100">
+                <a class="navbar-item has-text-weight-bold is-size-6" value="80">80 - 100</a>
+              </router-link>
+              <router-link to="/beers/ibu/100/300">
+                <a class="navbar-item has-text-weight-bold is-size-6" value="100">> 100</a>
+              </router-link>
+            </div>
           </div>
         </div>
       </div>
@@ -46,91 +76,6 @@
         </div>
       </div>
     </div>
-    <!-- <div class="navbar-brand">
-      <router-link class="navbar-item" to="/home">
-        <img src="../assets/brewdog-logo.png" alt="Brewdog logo" width="35" height="50">
-      </router-link>
-      <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-    </div>
-
-    <div class="navbar-menu">
-      <div class="navbar-start">
-        <div class="navbar-item is-hoverable">
-          <span class="navbar-link is-arrowless">
-            <router-link to="/home">
-              <div class="menu-title">Home</div>
-            </router-link>
-          </span>
-        </div>
-        <div class="navbar-item has-dropdown is-hoverable">
-          <span class="navbar-link">
-            <router-link to="/beers">
-              <div class="menu-title">Beers</div>
-            </router-link>
-          </span>
-          <div class="navbar-dropdown is-boxed">
-            <div class="columns">
-              <div class="column">
-                <div class="navbar-item has-text-weight-bold is-size-5">ABV</div>
-                <hr class="navbar-divider">
-                <router-link to="/beers/abv/0/3">
-                  <a class="navbar-item has-text-weight-bold is-size-6" value="0-3">0 - 3%</a>
-                </router-link>
-                <router-link to="/beers/abv/3/6">
-                  <a class="navbar-item has-text-weight-bold is-size-6" value="3-6">3 - 6%</a>
-                </router-link>
-                <router-link to="/beers/abv/6/9">
-                  <a class="navbar-item has-text-weight-bold is-size-6" value="6-10">6 - 9%</a>
-                </router-link>
-                <router-link to="/beers/abv/9/15">
-                  <a class="navbar-item has-text-weight-bold is-size-6" value="9-15">9 - 15%</a>
-                </router-link>
-                <router-link to="/beers/abv/15/35">
-                  <a class="navbar-item has-text-weight-bold is-size-6" value="15-25">15 - 35%</a>
-                </router-link>
-                <router-link to="/beers/abv/35/60">
-                  <a class="navbar-item has-text-weight-bold is-size-6" value="16">> 35%</a>
-                </router-link>
-              </div>
-              <div class="column">
-                <div class="navbar-item has-text-weight-bold is-size-5">IBU</div>
-                <hr class="navbar-divider">
-                <router-link to="/beers/ibu/0/20">
-                  <a class="navbar-item has-text-weight-bold is-size-6" value="0-20">0 - 20</a>
-                </router-link>
-                <router-link to="/beers/ibu/20/40">
-                  <a class="navbar-item has-text-weight-bold is-size-6" value="20">20 - 40</a>
-                </router-link>
-                <router-link to="/beers/ibu/40/60">
-                  <a class="navbar-item has-text-weight-bold is-size-6" value="40">40 - 60</a>
-                </router-link>
-                <router-link to="/beers/ibu/60/80">
-                  <a class="navbar-item has-text-weight-bold is-size-6" value="60">60 - 80</a>
-                </router-link>
-                <router-link to="/beers/ibu/80/100">
-                  <a class="navbar-item has-text-weight-bold is-size-6" value="80">80 - 100</a>
-                </router-link>
-                <router-link to="/beers/ibu/100/300">
-                  <a class="navbar-item has-text-weight-bold is-size-6" value="100">> 100</a>
-                </router-link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      
-      <div class="navbar-item is-hoverable">
-        <span class="navbar-link is-arrowless">
-          <router-link to="/home">
-            <div class="menu-title">Random</div>
-          </router-link>
-        </span>
-      </div>
-    </div>-->
   </div>
 </template>
 
@@ -156,6 +101,9 @@ export default {
 <style lang="scss">
 .navbar {
   background-color: lightgrey;
+  position: fixed;
+  top: 0;
+  width: 100%;
 }
 
 .menu-items {
@@ -174,10 +122,18 @@ export default {
   font-size: 20px;
   align-self: center;
   .menu-item-dropdown {
+    display: flex;
     position: absolute;
     background-color: whitesmoke;
-    border: 1px solid black;
+    border: 1px solid #2c3e50;
+    border-radius: 2%;
     padding: 16px;
+    align-items: left;
+  }
+  .arrow {
+    border-width: 1px 1px 0 0;
+    border-style: solid;
+    border-color: #2c3e50;
   }
 }
 
@@ -189,7 +145,19 @@ export default {
 }
 a {
   text-decoration: none;
-  color: black;
+  color: #2c3e50;
+  padding: 10px 0;
+}
+.abv,
+.ibu {
+  display: flex;
+  flex-direction: column;
+  .menu-item-title {
+    padding: 10px 0;
+  }
+}
+.ibu {
+  padding-left: 30px;
 }
 </style>
 
